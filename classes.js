027f63acf7ed18255fbc0bca44557b5d91c31174
Admiral General <muhammadusmanawan88@gmail.com>
2024-11-09 16:11:31 +0500

    Inheritance

--- classes.js
+++ classes.js
@@ -17,22 +17,50 @@ class User{
     {
         return this.#courseList;
     }
+    static login()  //? this is a static method, it belongs to the class itself, not to an instance of the class, nor it is inherited by other classes 
+    {
+        return "User logged in";
+    }
 
 }
-
 // let User1 = new User('John Wick', 'mr.wick@gmail.com');
 // console.log(User1);
+
+//* Continuing with the above code...
+// TODO: Classes and inheritance in JavaScript
+
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+    getAdminInfo() {
+        return "I am an admin";
+    }
+    login()   //? Overriding the login method from the parent class, and it will be called instead of the parent class's login method. when called when called from an instance of the child class, it will call the overridden method.
+    {
+        return "Admin logged in";
+    }
+}
 
 //* Exports User class
 module.exports = User;
 
 var User2 = new User('John Cena', 'john.cena@gmail.com');
-console.log(User2.getInfo());
+// console.log(User2.getInfo());
 User2.enrollCourse('C++');
 User2.enrollCourse('C#');
 User2.enrollCourse('Python');
-console.log(User2.getCourseList());
-console.log(User2.courseList);        // Accessing private property directly is not allowed, if "courseList" was not a private property this will work fine. just like the above line.
+// console.log(User2.getCourseList());
+// console.log(User2.courseList);        // Accessing private property directly is not allowed, if "courseList" was not a private property this will work fine. just like the above line.
+
+const tom = new Admin("Tom Hanks", "tom.hanks@gmail.com");
+console.log(tom.getAdminInfo());
+// console.log(tom.login());    //? static method can be accessed directly on the class itself, not on an instance of the class.
+
+console.log(tom.getInfo());
+console.log(User.login());     //? static method can only be called directly on the class itself, not on an instance of the class.
+console.log(tom.login());      //? Overridden method will be called instead of the parent class's login method. when called from an instance of the child class, it will call the overridden method. 
+
 
 
 
