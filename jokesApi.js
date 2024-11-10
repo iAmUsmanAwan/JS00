09ffab79ef2fa5d792a60e68d762e3c8069eb4c3
Admiral General <muhammadusmanawan88@gmail.com>
2024-11-11 01:58:41 +0500

    Jokes API

---
+++ jokesApi.js
@@ -0,0 +1,11 @@
+// TODO: Implement this in the Inspect section of the browser console as this will not work in Node.js environment
+
+fetch(`https://api.chucknorris.io/jokes/random`)
+.then((responseGot) => {  //? if the response is ok (status 200)
+    return responseGot.json();
+}) 
+.then((dataGotFromFirstThen) => {  //? if the promise from the first then is resolved
+    var Joke = dataGotFromFirstThen.value;
+    console.log("Joke fetched : ", Joke);    
+})
+.catch()  //? if any error occurred during the fetch request
