654727f11e049262c481b29f2df55098a934bed9
Usman Awan <97288095+iAmUsmanAwan@users.noreply.github.com>
2025-01-29 06:45:51 +0500

    Develop (#11)
    * Interview Practices
    
    * Revised
    
    * Updated
    
    * updated
    
    * REVISED
    
    * revising
    
    * Revised
    
    * REVISED
    
    * Revision
    
    * Revised
    
    * Revised
    
    * Revising
    
    * null Coalescing
    
    * new
    
    * New

--- dextor.js
+++ dextor.js
@@ -1829,8 +1829,26 @@ NaN                                                                             
 // console.log(isPrime(1));  // false
 
 
+// FIXME:
+//* Pyramid generator:
 
+// const character = "*";
+// const count = 8;
+// const rows = [];
 
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
+// for (let i = 0; i < count; i += 1) {   //? {i += 1} "take the original value of the variable, add this value, and assign the result back to the variable."
+//         rows.push(padRow(i + 1, count));
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//     result = result + row + "\n";
+// }
+// console.log(result);
 
 
 
