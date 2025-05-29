41065cb973be5544ed546bdaa84fa5be6c6c6c9e
Admiral General <muhammadusmanawan88@gmail.com>
2025-05-29 22:40:00 +0500

    API fetch and Promise Chaining

--- jokesApi.js
+++ jokesApi.js
@@ -1,13 +1,21 @@
 // TODO: Implement this in the Inspect section of the browser console but this will also work in Node.js environment
 
 fetch(`https://api.chucknorris.io/jokes/random`)
+
 .then((responseGot) => {  //? if the response is ok (status 200)
     return responseGot.json();
-}) 
-.then((dataGotFromFirstThen) => {  //? if the promise from the first then is resolved
-    console.log("Data is :" ,  dataGotFromFirstThen);
-    
-    var Joke = dataGotFromFirstThen.value;
-    console.log("Joke fetched : ", Joke);    
 })
-.catch()  //? if any error occurred during the fetch request
+
+.then((dataGotFromFirstThen) => {  //? if the promise from the first then is resolved
+    // console.log("Data is :" ,  dataGotFromFirstThen);
+    
+    // var Joke = dataGotFromFirstThen.value;
+    // console.log("Joke fetched : ", Joke);    
+
+    console.log("✅ Joke Data Received:", dataGotFromFirstThen);
+    console.log("🤣 Joke:", dataGotFromFirstThen.value);
+})
+
+.catch((error)=>{
+    console.error("❌ Error occurred while fetching the joke: ", error);
+})  //? if any error occurred during the fetch request
